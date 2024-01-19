1df99d76f3c79e85a8909da631229d0d197696a9
will-me-me <willykaria777@gmail.com>
2024-01-19 06:37:50 -0800

    updated the configurations

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install 2.1.0 version of flask from pip
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+# install flask from pip3
+
+exec { 'install flask':
+command => '/usr/bin/pip3 install flask==2.1.0'
 }

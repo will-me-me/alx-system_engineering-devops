4d969ce7e4a0944426e85d0e0b2185116a03e5cc
will-me-me <willykaria777@gmail.com>
2024-03-06 12:58:24 -0800

    updated and fixed bug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""
+get the top ten hot topics of a subreddit
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """
+    get the top ten hot topics of a subreddit
+    """
+    results = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+        headers={"User-Agent": "my bot 0.1"})
+    if results.status_code == 200:
+        response_data = results.json()
+        titles = response_data['data']['children']
+        for title in titles:
+            print(title['data']['title'])
+    else:
+        print(None)
